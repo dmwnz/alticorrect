5d5dd7e113d2d7e720a150703153e04c47375353
dmwnz <33037675+dmwnz@users.noreply.github.com>
2021-06-15 16:23:58 +0200

    set correct MIME type for fixed file download

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function fixGpxAltitudes(xml, altitudes) {
 
 function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', 'data:application/gpx+xml;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
 
     element.style.display = 'none';
